0e61505d0991b55b638871d116015cd186192089
rutger <36332815+rutger-t@users.noreply.github.com>
2021-04-01 07:45:10 +0900

    add todo new to index view

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,6 +4,7 @@ class TodosController < ApplicationController
   # GET /todos or /todos.json
   def index
     @todos = Todo.all
+    @todo = Todo.new
   end
 
   # GET /todos/1 or /todos/1.json
@@ -25,7 +26,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: "Todo was successfully created." }
+        format.html { redirect_to todos_url, notice: "Todo was successfully created." }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new, status: :unprocessable_entity }
