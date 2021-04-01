61142747402de2e677293f667e98c32de86ba398
rutger <36332815+rutger-t@users.noreply.github.com>
2021-04-01 18:12:22 +0900

    add turbo stream

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,7 @@
 class Todo < ApplicationRecord
+  after_create_commit { broadcast_prepend_to "todos" }
+  after_update_commit { broadcast_replace_to "todos" }
+  after_destroy_commit { broadcast_remove_to "todos" }
+
+  validates :task, presence: true
 end
